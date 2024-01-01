0577dd78023b08a3283c026d2f8aed2b5df30c53
snoussigaith <snoussi_gaith95@outlook.fr>
2024-01-01 15:29:16 +0100

    home component done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSelectChange } from '@angular/material/select';
+import { TodoComponent } from '../todo/todo.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  todos: any = [];
+  filteredTodos: any[] = [{title: 'Test', description: 'Test description', status: 'OPEN'}];
+  constructor(private apiService: ApiService,
+    private dialog: MatDialog) {
+}
+
+  ngOnInit(): void {
+    /* this.apiService.getAllTodos().subscribe((todos) => {
+      this.todos = todos;
+      this.filteredTodos = this.todos;
+    })*/
+
+  }
+
+  // tslint:disable-next-line:typedef
+  filterChanged(ev: MatSelectChange) {
+    const value = ev.value;
+    this.filteredTodos = this.todos;
+    if (value) {
+      this.filteredTodos = this.filteredTodos.filter(t => t.status === value);
+      console.log(this.filteredTodos);
+    } else {
+      this.filteredTodos = this.todos;
+    }
+  }
+  openDialog() {
+    const dialogRef = this.dialog.open(TodoComponent, {
+      width: '500px',
+      hasBackdrop: true,
+      role: 'dialog',
+      height: '500px'
+    });
+
+    dialogRef.afterClosed().subscribe(data => {
+      /*this.apiService.createTodo(data.title, data.description).subscribe((result: any) => {
+        console.log(result);
+        this.todos.push(result);
+        this.filteredTodos = this.todos;
+      })*/
+    });
+  }
+
+  // tslint:disable-next-line:typedef
+  statusChanged(ev: MatSelectChange, todoId: number, index: number) {
+    const value = ev.value;
+    /*this.apiService.updateStatus(value, taskId).subscribe(todo => {
+      this.todos[index] = todo;
+      this.filteredTodos = this.todos;
+    });*/
+  }
+
+  // tslint:disable-next-line:typedef
+  delete(id: number) {
+    if (confirm('Do you want to remove the Todo?')) {
+      /*this.apiService.deleteTodo(id).subscribe(res => {
+        if (res.success) {
+          this.todos = this.todos.filter((t: any) => t.id !== id);
+          this.filteredTodos = this.todos;
+        }
+      });*/
+    }
+  }
 
 }
